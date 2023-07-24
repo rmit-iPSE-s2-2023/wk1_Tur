fef51353f4361c63d7de7e3f96a99e74767a13f1
linruicheng <lexunbb@outlook.com>
2023-07-24 17:41:09 +1000

    Initial Commit

---
+++ wk1Tur/wk1TurApp.swift
@@ -0,0 +1,17 @@
+//
+//  wk1TurApp.swift
+//  wk1Tur
+//
+//  Created by 林锐城 on 24/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct wk1TurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
